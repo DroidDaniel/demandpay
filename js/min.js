2ce2936f9d0ca450ce0cfdbd93b808851a8169e8
Sri <sriar.srivati@ansira.com>
2023-10-24 09:20:43 -0700

    images compressed

--- js/min.js
+++ js/min.js
@@ -17,4 +17,30 @@ $(document).ready(function () {
     dots: true,
     pauseOnHover: true,
   });
+
+  //validation
+  var $form = $("form"),
+    $successMsg = $(".alert");
+  $.validator.addMethod("letters", function (value, element) {
+    return this.optional(element) || value == value.match(/^[a-zA-Z\s]*$/);
+  });
+  $form.validate({
+    rules: {
+      email: {
+        required: true,
+        email: true,
+      },
+      // message: {
+      //   required: true,
+      //   minlength: 20,
+      // },
+    },
+    messages: {
+      email: "Please Enter valid email address",
+      //message: "Please Enter your message",
+    },
+    submitHandler: function () {
+      $successMsg.css("opacity", "1");
+    },
+  });
 });
