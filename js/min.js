344e07c04e9dcdc07d0a9f22aeae49865e05507f
Sri <sriar.srivati@ansira.com>
2023-10-24 12:06:30 -0700

    google sheet

--- js/min.js
+++ js/min.js
@@ -1,13 +1,19 @@
 $(document).ready(function () {
+  //Hamburger menu
   $(".nav_menu").click(function () {
     $("header").toggleClass("navbar_mob");
   });
   $("nav a").click(function () {
     $("header").removeClass("navbar_mob");
   });
-});
 
-$(document).ready(function () {
+  //Add active class on nav links
+  $("nav a").click(function () {
+    $("nav a").removeClass("active");
+    $(this).addClass("active");
+  });
+
+  //Feedback Slider
   $(".customer-logos").slick({
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -20,13 +26,13 @@ $(document).ready(function () {
 
   //validation
   var $form = $("form"),
-    $successMsg = $(".alert");
+    $successMsg = $(".success_msg");
   $.validator.addMethod("letters", function (value, element) {
     return this.optional(element) || value == value.match(/^[a-zA-Z\s]*$/);
   });
   $form.validate({
     rules: {
-      email: {
+      "entry.90727839": {
         required: true,
         email: true,
       },
@@ -41,6 +47,20 @@ $(document).ready(function () {
     },
     submitHandler: function () {
       $successMsg.css("opacity", "1");
+      // Get the form data
+      var data = $("form").serialize();
+      $.ajax({
+        url: "https://docs.google.com/forms/u/0/d/e/1FAIpQLScTcTrFlZFB-YNgbx4hdlyK8aamI99twc0Xdr_ylAwr02UuwQ/formResponse",
+        type: "POST",
+        data: data,
+        success: function (response) {
+          console.log("Success");
+        },
+        error: function (error) {
+          console.log("Error");
+        },
+      });
+      $("#contact_form")[0].reset();
     },
   });
 });
